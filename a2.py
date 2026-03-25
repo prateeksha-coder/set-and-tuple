9f49441e19dd08180bc117a146b0beca694c119f
prateeksha-coder <prateeksha.codingal@gmail.com>
2026-03-25 12:07:19 +0530

    commit

--- a2.py
+++ a2.py
@@ -12,4 +12,4 @@ print("Set 2", set2)
 print("Difference")
 print(set1.difference(set2))#set1-set2
 print("Symmeteric Difference")
-print(set1.symmetric_difference(set2))
+print(set2.symmetric_difference(set1))
